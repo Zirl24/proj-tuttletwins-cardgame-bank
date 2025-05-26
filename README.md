8af03b9c97f9c8b2cca539fefcd758596cd5c37f
Abraham Hamilton <hamfam@Abrahams-MacBook-Pro.local>
2025-05-26 17:41:09 -0500

    final product

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # proj-ttwins-calc
+A simple project that keeps track the money that each player has in the Tuttle Twins Inflation Monster Card Game :)
--- ttwins.py
+++ ttwins.py
@@ -1,12 +1,37 @@
 contin = True
-money = {}
+players = {}
 
 while contin:
     player = input("Add Player: ")
-    if player == "start":
+    if player == "start" or player == "":
         contin = False
     else:
-        money[player] = 4
+        players[player] = 4
         print(f"added {player}")
 
+print("Game Beginning with players:")
+for name in players:
+    print(name)
+
+input("----------")
 contin = True
+
+def view_all():
+    for name, balance in players.items():
+            print(f"{name}: {balance}")
+
+while contin:
+    currentPlayer = input("View which player: ")
+    if currentPlayer == "end":
+        contin = False
+        print("SESSION TERMINATED")
+    elif currentPlayer == "all":
+        view_all()
+    elif currentPlayer in players:
+        print(f"{currentPlayer}: {players[currentPlayer]}k")
+        calc = input("Increase/Decrease balance by: ")
+        if not calc == "":
+            players[currentPlayer] += int(calc)
+            print(f"New Balance: {players[currentPlayer]}k")
+    else:
+        print("404: Player not found")
